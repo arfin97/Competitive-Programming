14b54d96a5187761036dd58f9848630a8cdb0480
arfin97 <arfin97@gmail.com>
2017-09-07 18:06:02 +0600

    Add files via upload

--- URI/1566 - Height.cpp
+++ URI/1566 - Height.cpp
@@ -1,39 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define debug(x) cout << #x << " = " << (x) << endl
-#define fr freopen("in.txt", "r", stdin);
-#define fw freopen("out.txt", "w", stdout);
-#define mem(x) memset((x), 0, sizeof((x)));
 
 int ara[3000000];
 
 int main(){
-	string str;
-    while(getline(cin, str)){
-	    if(str == "0") break;
-	    stringstream ss(str);
-	    int x = 0;
-	    int num;
-	    while(ss >> num){
-	    	ara[x] = num;
-	    	x++;
-	    }
+    int tc;
+    scanf("%d", &tc);
+    for(int tr = 1; tr <= tc; tr++){
+        int n;
+        scanf("%d", &n);
+        for(int i = 0; i < n; i++) scanf("%d", &ara[i]);
+        sort(ara, ara+n);
+        int first_print = 0;
+        for(int i = 0; i < n; i++){
+            if(first_print == 0) printf("%d", ara[i]);
+            else printf(" %d", ara[i]);
+            first_print = 1;
+        }
+        printf("\n");
 
-	    int cnt = 0;
-	    for(int i = 0; i < x; i++){
-	        for(int j = 0; j < x; j++){
-	            if(ara[i] >= ara[j]){
-	                swap(ara[i], ara[j]);
-	                cnt++;
-	            }
-	        }
-	    }
-
-	    if(cnt%2 == 0){
-	    	printf("Marcelo\n");
-	    }else{
-	    	printf("Carlos\n");
-	    }
     }
     return 0;
 }
